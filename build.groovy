3ff4d4603dbe72441b7d63e5294722f39a63569e
Lars Bilke <lars.bilke@ufz.de>
2015-08-03 11:09:59 +0200

    Switching to latest tag.

--- build.groovy
+++ build.groovy
@@ -3,17 +3,17 @@ node('docker') {
 	git 'https://github.com/ufz/dockerfiles'
 
 	stage "Building GCC images"
-	docker.build "ogs6/gcc-dev-essentials:${env.BUILD_TAG}", "gcc/dev-essentials"
-	docker.build "ogs6/gcc-ogs-base:${env.BUILD_TAG}", "gcc/ogs-base"
-	docker.build "ogs6/gcc-ogs-cli:${env.BUILD_TAG}", "gcc/ogs-cli"
-	docker.build "ogs6/gcc-ogs-gui:${env.BUILD_TAG}", "gcc/ogs-gui"
-	docker.build "ogs5/gcc-ogs-base:${env.BUILD_TAG}", "gcc/ogs5-base"
+	docker.build "ogs6/gcc-dev-essentials:latest", "gcc/dev-essentials"
+	docker.build "ogs6/gcc-ogs-base:latest", "gcc/ogs-base"
+	docker.build "ogs6/gcc-ogs-cli:latest", "gcc/ogs-cli"
+	docker.build "ogs6/gcc-ogs-gui:latest", "gcc/ogs-gui"
+	docker.build "ogs5/gcc-ogs-base:latest", "gcc/ogs5-base"
 
 	stage "Building Clang images"
-	docker.build "ogs6/clang-dev-essentials:${env.BUILD_TAG}", "clang/dev-essentials"
+	docker.build "ogs6/clang-dev-essentials:latest", "clang/dev-essentials"
 
 	stage "Building MinGW images"
-	docker.build "ogs6/mingw-dev-essentials:${env.BUILD_TAG}", "mingw/dev-essentials"
-	docker.build "ogs5/mingw-ogs-base:${env.BUILD_TAG}", "mingw/ogs5-base"
-	docker.build "ogs6/mingw-ogs-gui:${env.BUILD_TAG}", "mingw/ogs-gui"
+	docker.build "ogs6/mingw-dev-essentials:latest", "mingw/dev-essentials"
+	docker.build "ogs5/mingw-ogs-base:latest", "mingw/ogs5-base"
+	docker.build "ogs6/mingw-ogs-gui:latest", "mingw/ogs-gui"
 }

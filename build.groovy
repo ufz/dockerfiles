ad353af5a03ffc7db897038bd8f317f6eb507713
Lars Bilke <lars.bilke@ufz.de>
2016-01-15 13:39:37 +0100

    Disable pushing for now.

--- build.groovy
+++ build.groovy
@@ -6,23 +6,23 @@ node('docker') {
 
 		stage "Building GCC images"
 		def image = docker.build "ogs6/gcc-dev-essentials:latest", "gcc/dev-essentials"
-		image.push()
+		//image.push()
 		image = docker.build "ogs6/gcc-ogs-base:latest", "gcc/ogs-base"
-		image.push()
+		//image.push()
 		image = docker.build "ogs6/gcc-ogs-gui:latest", "gcc/ogs-gui"
-		image.push()
+		//image.push()
 		image = docker.build "ogs5/gcc-ogs-base:latest", "gcc/ogs5-base"
-		image.push()
+		//image.push()
 
 		stage "Building Clang images"
 		image = docker.build "ogs6/clang-dev-essentials:latest", "clang/dev-essentials"
-		image.push()
+		//image.push()
 		image = docker.build "ogs6/clang-ogs-base:latest", "clang/ogs-base"
-		image.push()
+		//image.push()
 
 		stage "Building MinGW images"
 		image = docker.build "ogs6/mingw-dev-essentials:latest", "mingw/dev-essentials"
-		image.push()
+		//image.push()
 		image = docker.build "ogs5/mingw-gcc:latest", "mingw/gcc"
 		// image.push()
 		image = docker.build "ogs6/mingw-ogs-gui:latest", "mingw/ogs-gui"
@@ -31,5 +31,7 @@ node('docker') {
 		// image.push()
 		image = docker.build "ogs6/mingw-32-ogs-base:latest", "mingw-32/ogs-base"
 		// image.push()
+
+		//stage "Push images"
 	}
 }

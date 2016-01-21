86fced74d11f5d210ca0f37db4400f9797badade
Lars Bilke <lars.bilke@ufz.de>
2016-01-21 17:56:50 +0000

    Added images using prebuilt mxe packages.

---
+++ mingw-bin/build.sh
@@ -0,0 +1,2 @@
+docker build -t ogs6/mingw-bin-dev-essentials:latest dev-essentials
+docker build -t ogs6/mingw-bin-ogs-base:latest ogs-base
---
+++ mingw-bin/push.sh
@@ -0,0 +1,2 @@
+docker push ogs6/mingw-bin-dev-essentials:latest
+docker push ogs6/mingw-bin-ogs-base:latest

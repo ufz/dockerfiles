3d00de412d03470575ccafdf229493c05ef71add
Lars Bilke <lars.bilke@ufz.de>
2016-11-07 14:43:45 +0100

    Added latex image with ghostscript.

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 (cd $DIR/clang && ./build.sh)
 (cd $DIR/gcc && ./build.sh)
 (cd $DIR/mingw && ./build.sh)
+(cd $DIR/misc && ./build.sh)
--- push.sh
+++ push.sh
@@ -2,3 +2,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 $DIR/clang/push.sh
 $DIR/gcc/push.sh
 $DIR/mingw/push.sh
+$DIR/misc/push.sh

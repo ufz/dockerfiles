e3dee89d4b90c8254a7ad4514ded74e4dd2f47a0
Lars Bilke <lars.bilke@ufz.de>
2016-11-08 12:54:16 +0100

    Fixed typos.

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-$DIR/clang/push.sh
-$DIR/gcc/push.sh
-$DIR/mingw/push.sh
-$DIR/misc/push.sh
+(cd $DIR/clang && ./push.sh)
+(cd $DIR/gcc && ./push.sh)
+(cd $DIR/mingw && ./push.sh)
+(cd $DIR/misc && ./push.sh)

5b624c6e6376e79e818d58babdb3912fce1638b6
Lars Bilke <lars.bilke@ufz.de>
2016-11-29 11:50:43 +0100

    Added image ogs6/gcc-latex

--- gcc/build.sh
+++ gcc/build.sh
@@ -2,4 +2,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 docker build -t ogs6/gcc-dev-essentials:latest $DIR/dev-essentials
 docker build -t ogs6/gcc-base:latest $DIR/base
 docker build -t ogs6/gcc-gui:latest $DIR/gui
+docker build -t ogs6/gcc-latex:latest $DIR/latex
 docker build -t ogs5/gcc-base:latest $DIR/ogs5-base
--- gcc/push.sh
+++ gcc/push.sh
@@ -1,5 +1,6 @@
 docker push ogs6/gcc-dev-essentials:latest
 docker push ogs6/gcc-base:latest
 docker push ogs6/gcc-gui:latest
+docker push ogs6/gcc-latex:latest
 
 docker push ogs5/gcc-base:latest

84fb4bf8192ca7c5c5b49aa41186b3bf196a124a
Lars Bilke <lars.bilke@ufz.de>
2016-12-23 09:17:17 +0100

    Added image ogs6/gcc-conan-package-tools.

--- gcc/push.sh
+++ gcc/push.sh
@@ -2,5 +2,6 @@ docker push ogs6/gcc-dev-essentials:latest
 docker push ogs6/gcc-base:latest
 docker push ogs6/gcc-gui:latest
 docker push ogs6/gcc-latex:latest
+docker push ogs6/gcc-conan-package-tools:latest
 
 docker push ogs5/gcc-base:latest

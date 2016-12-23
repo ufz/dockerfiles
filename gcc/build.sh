61735826effdb0a061dd8b34279bac4c97f99377
Lars Bilke <lars.bilke@ufz.de>
2016-12-23 13:53:48 +0100

    Added image gcc-conan-package-tools-qt.

--- gcc/build.sh
+++ gcc/build.sh
@@ -4,4 +4,5 @@ docker build -t ogs6/gcc-base:latest $DIR/base
 docker build -t ogs6/gcc-gui:latest $DIR/gui
 docker build -t ogs6/gcc-latex:latest $DIR/latex
 docker build -t ogs6/gcc-conan-package-tools:latest $DIR/conan-package-tools
+docker build -t ogs6/gcc-conan-package-tools-qt:latest $DIR/conan-package-tools-qt
 docker build -t ogs5/gcc-base:latest $DIR/ogs5-base

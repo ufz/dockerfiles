3b015e216be2096946496eba60f585049c986620
Lars Bilke <lars.bilke@ufz.de>
2017-03-15 15:04:34 +0100

    Added Conan gcc 4.6 and 4.8 images.

--- conan/build.sh
+++ conan/build.sh
@@ -1,2 +1,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+docker build -t ogs6/conangcc46:latest $DIR/gcc_4.6
+docker build -t ogs6/conangcc48:latest $DIR/gcc_4.8
 docker build -t ogs6/conangcc49:latest $DIR/gcc_4.9
--- conan/push.sh
+++ conan/push.sh
@@ -1,1 +1,3 @@
+docker push ogs6/conangcc46:latest
+docker push ogs6/conangcc48:latest
 docker push ogs6/conangcc49:latest

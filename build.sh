6b54f8b9ea6d3cb7ec772b778043832e3e6d2755
Lars Bilke <lars.bilke@ufz.de>
2017-04-19 15:20:19 +0200

    Typos.

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-docker build -t ogs6/gcc-dev-essentials:latest -f $DIR/dev-essentials/Dockerfile $DIR
+docker build -t ogs6/gcc-dev-essentials:latest -f $DIR/gcc/dev-essentials/Dockerfile $DIR
 docker build -t ogs6/gcc-base:latest -f $DIR/gcc/base/Dockerfile $DIR
 (cd $DIR/gcc && ./build.sh)
 
-docker build -t ogs6/clang-dev-essentials:latest -f $DIR/dev-essentials/Dockerfile $DIR
-docker build -t ogs6/clang-base:latest -f $DIR/base/Dockerfile $DIR
+docker build -t ogs6/clang-dev-essentials:latest -f $DIR/clang/dev-essentials/Dockerfile $DIR
+docker build -t ogs6/clang-base:latest -f $DIR/clang/base/Dockerfile $DIR
 
 (cd $DIR/conan && ./build.sh)
 (cd $DIR/mingw && ./build.sh)

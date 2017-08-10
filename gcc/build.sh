588b27c08592c25844e74d9c5c3bee3637c24735
Lars Bilke <lars.bilke@ufz.de>
2017-08-10 08:40:56 +0200

    Moved latex biber package to gcc-gui image.

--- gcc/build.sh
+++ gcc/build.sh
@@ -1,7 +1,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 docker build -t ogs6/gcc-conan:latest $DIR/conan
 docker build -t ogs6/gcc-gui:latest $DIR/gui
-docker build -t ogs6/gcc-latex:latest $DIR/latex
 docker build -t ogs6/gcc-conan-package-tools:latest $DIR/conan-package-tools
 docker build -t ogs6/gcc-conan-package-tools-qt:latest $DIR/conan-package-tools-qt
 docker build -t ogs5/gcc-base:latest $DIR/ogs5-base

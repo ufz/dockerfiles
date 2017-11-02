c7581382220b01eb2da3bf910e78167c3db9d5db
Lars Bilke <lars.bilke@ufz.de>
2017-11-02 15:48:45 +0100

    Use Github boost download link.

--- scripts/install-boost.sh
+++ scripts/install-boost.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-curl -L -o boost_1_56_0.tar.bz2 https://ogsstorage.blob.core.windows.net/jenkins/ogs6-lib-sources/boost_1_56_0.tar.bz2 && \
-  tar -xjf boost_1_56_0.tar.bz2 && \
-  chmod 755 boost_1_56_0 && \
+curl -L -o boost_1_56_0.tar.gz https://github.com/boostorg/boost/archive/boost-1.56.0.tar.gz && \
+  tar -xf boost_1_56_0.tar.gz && \
+  chmod 755 boost-boost-1.56.0 && \
+  mv boost-boost-1.56.0 boost_1_56_0
   mv boost_1_56_0 $1 && \
-  rm boost_1_56_0.tar.bz2
+  rm boost_1_56_0.tar.gz

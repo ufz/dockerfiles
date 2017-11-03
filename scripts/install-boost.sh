8b264d6fcb749da90b5beb4e581de12d67b3daf6
Lars Bilke <lars.bilke@ufz.de>
2017-11-03 12:28:20 +0100

    Use system boost (1.58).

--- scripts/install-boost.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-curl -L -o boost_1_56_0.tar.gz https://github.com/boostorg/boost/archive/boost-1.56.0.tar.gz && \
-  tar -xf boost_1_56_0.tar.gz && \
-  chmod 755 boost-boost-1.56.0 && \
-  mv boost-boost-1.56.0 boost_1_56_0
-  mv boost_1_56_0 $1 && \
-  rm boost_1_56_0.tar.gz

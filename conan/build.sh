99a4625cec3138a782b5b1b76c146beab3cd4130
Lars Bilke <lars.bilke@ufz.de>
2017-12-15 17:13:30 +0100

    Added lasote/conangcc5 Jenkins extension.

--- conan/build.sh
+++ conan/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+docker build $1 -t ogs6/conangcc5:latest $DIR/gcc_5
+
 docker build $1 -t ogs6/conangcc46:latest $DIR/gcc_4.6
 docker build $1 -t ogs6/conangcc48:latest $DIR/gcc_4.8
 docker build $1 -t ogs6/conangcc49:latest $DIR/gcc_4.9
--- conan/push.sh
+++ conan/push.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+docker push ogs6/conangcc5:latest
+
 docker push ogs6/conangcc46:latest
 docker push ogs6/conangcc48:latest
 docker push ogs6/conangcc49:latest

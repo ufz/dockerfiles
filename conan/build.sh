901947b58f78c49f55010177378cfb5356676173
Lars Bilke <lars.bilke@ufz.de>
2017-12-18 14:59:59 +0100

    Updated ogs6/conangcc49.

--- conan/build.sh
+++ conan/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+docker build $1 -t ogs6/conangcc49:latest $DIR/gcc_4.9
 docker build $1 -t ogs6/conangcc5:latest $DIR/gcc_5
 docker build $1 -t ogs6/conangcc6:latest $DIR/gcc_6
 docker build $1 -t ogs6/conangcc7:latest $DIR/gcc_7
-
-docker build $1 -t ogs6/conangcc49:latest $DIR/gcc_4.9
--- conan/push.sh
+++ conan/push.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
+docker push ogs6/conangcc49:latest
 docker push ogs6/conangcc5:latest
 docker push ogs6/conangcc6:latest
 docker push ogs6/conangcc7:latest
-
-docker push ogs6/conangcc49:latest
